2ec4ace608299747d890bd5278ac14a80fb59205
AdrshChaudhary <im.aadrsh@gmail.com>
2025-07-09 22:36:56 +0530

    test

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ import os
 import re
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 # ===============================
 # MongoDB Configuration
